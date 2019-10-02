237522ab24f0523c564df724439e99a2a975fc2c
Kylan <aviananalyst@gmail.com>
2019-10-02 03:57:42 -0700

    Update frontend to allow users to pick both dates

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="app" class="container">
-        <ReportPicker v-on:change="updateDate" />
+        <h1>Investments through:</h1>
+        <ReportPicker v-on:change="updateReportDate" />
+        <h1>Data through:</h1>
+        <ReportPicker v-on:change="updateInfoDate" />
         <Table v-bind:data="investments" class="col-md"/>
     </div>
 </template>
@@ -16,16 +19,25 @@
             ReportPicker
         },
         methods: {
-            async updateDate(date) {
-                this.date = date;
+            async updateReportDate(date) {
+                this.report_date = date;
+                // eslint-disable-next-line no-console
+                console.log('updating investments');
+                await this.updateInvestments()
+            },
+            async updateInfoDate(date) {
+                this.info_date = date;
                 // eslint-disable-next-line no-console
                 console.log('updating investments');
                 await this.updateInvestments()
             },
             async updateInvestments() {
                 let url = 'http://localhost:8000/investments';
-                if (this.date) {
-                    url += '?investment_date=' + this.date;
+                if (this.report_date) {
+                    url += '?investment_date=' + this.report_date + '&';
+                }
+                if (this.info_date) {
+                    url += 'info_date=' + this.info_date;
                 }
                 const investments = await fetch(url);
                 this.investments = await investments.json();
@@ -34,11 +46,31 @@
                 // eslint-disable-next-line no-console
                 console.log('updating investments with ' + url)
             },
+            makeUrl() {
+                switch (this.info_date + this.report_date) {
+                    case 0:
+                        this.url = 'http://localhost:8000/investments';
+                        break;
+                    case 1:
+                        this.url = 'http://localhost:8000/investments?';
+                        if (this.report_date) {
+                            this.url += 'investment_date=' + this.report_date;
+                        } else {
+                            this.url += 'info_date=' + this.info_date;
+                        }
+                        break;
+                    case 2:
+                        this.url = 'http://localhost:8000/investments?investment_date=' +
+                                    this.report_date + '&info_date=' + this.info_date;
+                }
+            }
         },
         data(){
             return {
                 investments: null,
-                date: null,
+                report_date: null,
+                info_date: null,
+                url: null,
             };
         },
         async created() {
